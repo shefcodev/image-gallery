695b11ab0a661422f15f23db7bece9f7896f958c
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-02-21 15:26:40 +0100

    fetched data from the pixabay api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState, useEffect } from 'react';
+
+import ImageCard from './components/ImageCard';
+
 function App() {
-  return (
-    <div>
-      <p className='text-6xl underline text-green-900'>Hello World</p>
-    </div>
-  );
+  const [image, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await fetch(
+          `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
+        );
+
+        if (!response.ok) {
+          throw new Error('error');
+        }
+
+        return await response.json();
+      } catch (error) {
+        return error.message;
+      }
+    };
+
+    fetchImage().then((response) => {
+      setImages(response.hits);
+      setIsLoading(false);
+    });
+  }, [term]);
+
+  return <ImageCard />;
 }
 
 export default App;
